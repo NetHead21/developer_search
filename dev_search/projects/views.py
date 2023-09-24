8298ab92e9a0cbbd8d94a32aebc488e5b1b5e70f
Juniven <juniven.s@egrowthpartners.com>
2023-09-24 23:37:24 +0800

    Adding and Customizing User Registration Form

--- dev_search/projects/views.py
+++ dev_search/projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
@@ -23,6 +25,7 @@ def project(request, pk):
     return render(request, "projects/single_project.html", context)
 
 
+@login_required(login_url="login")
 def create_project(request):
     form = ProjectForm()
 
@@ -36,6 +39,7 @@ def create_project(request):
     return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def update_project(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -50,6 +54,7 @@ def update_project(request, pk):
     return render(request, "projects/project_form.html", context)
 
 
+@login_required(login_url="login")
 def delete_project(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == "POST":
--- dev_search/users/views.py
+++ dev_search/users/views.py
@@ -1,5 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 from .models import Profile
+from django.contrib import messages
+from .forms import CustomUserCreationForm
+
+
+def login_user(request):
+    page = "login"
+    if request.user.is_authenticated:
+        return redirect("profiles")
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "Username does not exist")
+
+        if user := authenticate(request, username=username, password=password):
+            login(request, user)
+            return redirect("profiles")
+        else:
+            messages.error(request, "Username or password is incorrect! ")
+
+    return render(request, "users/login_register.html")
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "User successfully logged out!  ")
+    return redirect("login")
+
+
+def register_user(request):
+    page = "register"
+    form = CustomUserCreationForm
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            return create_user(form, request)
+        else:
+            messages.error(request, "An error has occurred during registration.")
+
+    context = {
+        "page": page,
+        "form": form,
+    }
+    return render(request, "users/login_register.html", context)
+
+
+# TODO Rename this here and in `register_user`
+def create_user(form, request):
+    user = form.save(commit=False)
+    user.username = user.username.lower()
+    user.save()
+
+    messages.success(request, "User account as created successfully.")
+
+    login(request, user)
+    return redirect("profiles")
 
 
 # Create your views here.

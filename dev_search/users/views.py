6db00fda8d0c33005cb0462e099972798a921fb1
Juniven <juniven.s@egrowthpartners.com>
2023-10-05 06:24:11 +0800

    Add Message User and View Massages in User Inbox Functionality.

--- dev_search/users/views.py
+++ dev_search/users/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
-from .models import Profile
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
+from .models import Profile, Message
 from .utils import search_profiles, get_range_paginator
 
 
@@ -169,3 +169,52 @@ def delete_skill(request, pk):
         return redirect("account")
     context = {"object": skill}
     return render(request, "delete_template.html", context)
+
+
+@login_required(login_url="login")
+def inbox(request):
+    profile = request.user.profile
+    message_requests = profile.messages.all()
+    unread_count = message_requests.filter(is_read=False).count()
+    context = {"message_request": message_requests, "unread_count": unread_count}
+    return render(request, "users/inbox.html", context)
+
+
+@login_required(login_url="login")
+def view_message(request, pk):
+    profile = request.user.profile
+    message = profile.messages.get(id=pk)
+    if not message.is_read:
+        message.is_read = True
+        message.save()
+    context = {"message": message}
+    return render(request, "users/message.html", context)
+
+
+def create_message(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+
+            message.save()
+
+            messages.success(request, "Message successfully sent.")
+            return redirect("user-profile", pk=recipient.id)
+
+    context = {"recipient": recipient, "form": form}
+    return render(request, "users/message_form.html", context)

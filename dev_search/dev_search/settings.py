723351d8e1b49ba266fdf9a8d800b2bc0c83449b
Juniven <juniven.s@egrowthpartners.com>
2023-10-09 04:37:34 +0800

    Adding Forget Password or Reset Password via Email feature

--- dev_search/dev_search/settings.py
+++ dev_search/dev_search/settings.py
@@ -120,15 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp-mail.outlook.com"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get("OUTLOOK_EMAIL")
-# EMAIL_HOST_PASSWORD = os.environ.get("OUTLOOK_PASSWORD")
-
-EMAIL_HOST_USER = "junivensaavedra.test@gmail.com"
-EMAIL_HOST_PASSWORD = "skhaoadtonfuheiv"
+EMAIL_HOST_USER = os.environ.get("GMAIL_TEST_EMAIL")
+EMAIL_HOST_PASSWORD = os.environ.get("GMAIL_TEST_PASSWORD")
 
 STATIC_URL = "static/"
 MEDIA_URL = "/images/"
